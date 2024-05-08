4452d9f0c575aeaff036d2a46e27d7460b14c397
Polina <93955709+minlaux@users.noreply.github.com>
2024-05-08 19:22:20 -0400

    importing data

--- run_parkinsons.py
+++ run_parkinsons.py
@@ -1,1 +1,17 @@
 # Evaluating k-NN and Neural Network algorithms on parkinsons dataset
+
+import sys
+sys.path.append("./src/neural_net")
+sys.path.append("./src/k-NN")
+
+from NeuralNet import *
+from kNN import *
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.utils import shuffle
+from tabulate import tabulate
+import random
+
+# load parkinsons dataset
+import_data = pd.read_csv("src/datasets/parkinsons.csv")
