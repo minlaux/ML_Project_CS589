eea7d919c6c22c10e7b35cdadb0fdaae1b5fd6cd
Yan Mazheika <moyanchik1@gmail.com>
2024-05-08 21:19:32 -0400

    structure updates

--- src/titanic_preprocessing.py
+++ src/titanic_preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-titanic = pd.read_csv('./titanic.csv')
+titanic = pd.read_csv('./datasets/titanic.csv')
 
 titanic['Title'] = titanic['Name'].str.extract(r'^(\w+)', expand=False)
 
@@ -12,4 +12,4 @@ titanic = titanic.drop(columns={'Name','Title','Sex'})
 
 titanic = pd.concat([titanic, coded], axis=1)
 
-titanic.to_csv('titanic_processed.csv', index=False)
+titanic.to_csv('./datasets/titanic_processed.csv', index=False)
