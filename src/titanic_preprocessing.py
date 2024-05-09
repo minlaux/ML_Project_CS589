7df0b2ffe33e7f118d4db6046794e47d8eec12b7
Yan Mazheika <moyanchik1@gmail.com>
2024-05-08 21:59:05 -0400

    report updates

--- src/titanic_preprocessing.py
+++ src/titanic_preprocessing.py
@@ -2,14 +2,17 @@ import pandas as pd
 
 titanic = pd.read_csv('./datasets/titanic.csv')
 
-titanic['Title'] = titanic['Name'].str.extract(r'^(\w+)', expand=False)
+# titanic['Title'] = titanic['Name'].str.extract(r'^(\w+)', expand=False)
 
-coded = pd.get_dummies(titanic['Title'], prefix='title', dtype=int)
+# coded = pd.get_dummies(titanic['Title'], prefix='title', dtype=int)
 
 titanic['Male'] = titanic['Sex'].map({'male': 1, 'female': 0})
 
-titanic = titanic.drop(columns={'Name','Title','Sex'})
+# titanic = titanic.drop(columns={'Name','Title','Sex'})
+titanic = titanic.drop(columns={'Sex', 'Name'})
 
-titanic = pd.concat([titanic, coded], axis=1)
+# titanic = pd.concat([titanic, coded], axis=1)
+
+titanic = titanic.rename(columns={'Survived': 'class'})
 
 titanic.to_csv('./datasets/titanic_processed.csv', index=False)
