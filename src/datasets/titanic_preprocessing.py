b355c2aafee981cbcced7b9eb184a2400291bece
Yan Mazheika <moyanchik1@gmail.com>
2024-05-08 21:10:49 -0400

    pre-processed titanic dataframe

--- src/datasets/titanic_preprocessing.py
+++ src/datasets/titanic_preprocessing.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-titanic = pd.read_csv('titanic.csv')
+titanic = pd.read_csv('./titanic.csv')
 
-titanic['Name'] = titanic['Name'].str.replace(r'[^\w\s]', '')
+titanic['Title'] = titanic['Name'].str.extract(r'^(\w+)', expand=False)
+
+coded = pd.get_dummies(titanic['Title'], prefix='title', dtype=int)
+
+titanic['Male'] = titanic['Sex'].map({'male': 1, 'female': 0})
+
+titanic = titanic.drop(columns={'Name','Title','Sex'})
+
+titanic = pd.concat([titanic, coded], axis=1)
+
+titanic.to_csv('titanic_processed.csv', index=False)
